a0ea246108a52945bc9bd3b78dbf617bd4faf777
Karl <tekamkarl@gmail.com>
2024-01-18 02:51:00 +0100

    editing add

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,7 @@ class FileStorage:
             if key.split('.')[0] == cls_name:
                 new_dict[key] = self.__objects[key]
                 
-        return self.__objects
+        return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
